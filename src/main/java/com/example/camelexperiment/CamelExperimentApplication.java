f42fd7c01d2e4b126c5f9df417c1c913c48402ec
Manga <mail.mangala@gmail.com>
2023-05-24 06:28:17 +0530

    Initial commit

---
+++ src/main/java/com/example/camelexperiment/CamelExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.camelexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camelexperiment/CamelExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camelexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
