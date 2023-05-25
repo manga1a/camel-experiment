1e95f4b5a42ddfb013363a9d9708c102c7f34cee
Manga <mail.mangala@gmail.com>
2023-05-25 10:07:58 +0530

    Header based routing.

--- src/main/java/com/example/camelexperiment/routes/MessageRouter.java
+++ src/main/java/com/example/camelexperiment/routes/MessageRouter.java
@@ -9,9 +9,31 @@ public class MessageRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("direct:start")
-                .routeId("greetings-route")
-                .setBody(constant("Hello from Apache Camel!"))
+        from("direct:b2be.poa")
+                .routeId("b2be.poa")
+                .setBody(constant("This is B2BE POA route"))
+                .to("stream:out");
+
+        from("direct:b2be.asn")
+                .routeId("b2be.asn")
+                .setBody(constant("This is B2BE ASN route"))
+                .to("stream:out");
+
+        from("direct:b2be.invoice")
+                .routeId("b2be.invoice")
+                .setBody(constant("This is B2BE Invoice route"))
+                .to("stream:out");
+
+        from("direct:b2be")
+                .routeId("b2be")
+                .choice()
+                .when(simple("${header.messageType} == 'poa'"))
+                .to("direct:b2be.poa")
+                .when(simple("${header.messageType} == 'asn'"))
+                .to("direct:b2be.asn")
+                .when(simple("${header.messageType} == 'invoice'"))
+                .to("direct:b2be.invoice")
+                .otherwise()
                 .to("stream:out");
     }
 }
--- src/test/java/com/example/camelexperiment/routes/MessageRouterTest.java
+++ src/test/java/com/example/camelexperiment/routes/MessageRouterTest.java
@@ -5,6 +5,7 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.MockEndpoints;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,11 +21,34 @@ class MessageRouterTest {
     @EndpointInject("mock:stream:out")
     private MockEndpoint mock;
 
-    @Test
-    void whenSendBody_thenGreetingReceivedSuccessfully() throws InterruptedException {
-        mock.expectedBodiesReceived("Hello from Apache Camel!");
+    @BeforeEach
+    void beforeEach() {
+        mock.reset();
+    }
 
-        template.sendBody("direct:start", null);
+    @Test
+    void whenPoaMessageType_thenPoaMessageReceived() throws InterruptedException {
+        mock.expectedBodiesReceived("This is B2BE POA route");
+
+        template.sendBodyAndHeader("direct:b2be", null, "messageType", "poa");
+
+        mock.assertIsSatisfied();
+    }
+
+    @Test
+    void whenAsnMessageType_thenAsnMessageReceived() throws InterruptedException {
+        mock.expectedBodiesReceived("This is B2BE ASN route");
+
+        template.sendBodyAndHeader("direct:b2be", null, "messageType", "asn");
+
+        mock.assertIsSatisfied();
+    }
+
+    @Test
+    void whenInvoiceMessageType_thenInvoiceMessageReceived() throws InterruptedException {
+        mock.expectedBodiesReceived("This is B2BE Invoice route");
+
+        template.sendBodyAndHeader("direct:b2be", null, "messageType", "invoice");
 
         mock.assertIsSatisfied();
     }
